f061e02d494ac429ca1adfbdc9ce825a909b07e6
no_name <no@mail>
2020-07-26 20:46:42 +0200

    I'll explain when you're older!

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
 const express = require('express');
-const fs = require('fs')
-
+const fs = require('fs');
 const app = express();
+app.use(express.json());
+
 const spaces = JSON.parse(fs.readFileSync(`${__dirname}/data/spaces.json`));
 app.get('/api/v1/spaces', (req, res) => {
-  res.status(200).json(
-      {
-          status: 'success',
-          results : spaces.length,
-          data:
-              {
-                  spaces
-              }
-      }
-  )
-})
+  res.status(200).json({
+    status: 'success',
+    results: spaces.length,
+    data: {
+      spaces,
+    },
+  });
+});
+
+app.get('/api/v1/spaces/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > spaces.length) {
+    res.status(404).send({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  const space = spaces.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      space,
+    },
+  });
+  res.send();
+});
+
+app.post('/api/v1/spaces/', (req, res) => {
+  const space = req.body;
+  space.id = spaces[spaces.length - 1].id + 1;
+  spaces.push(space);
+  fs.writeFile(
+    `${__dirname}/data/spaces.json`,
+    JSON.stringify(spaces),
+    (err) => {
+      if (err)
+        res.status(500).json({
+          status: 'error',
+          data: 'none',
+        });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          space,
+        },
+      });
+    }
+  );
+});
 const port = 3000;
 app.listen(port, () => {
   console.log('cowork.io is under construction...');
