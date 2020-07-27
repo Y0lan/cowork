d6974f3a426ab119ee0a865167e34f6ed1573d9b
no_name <no@mail>
2020-07-27 13:15:09 +0200

    yolo push

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+const database = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.PASSWORD
+);
+
+mongoose.connect(database, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+}).then(connection => {
+  console.log(connection.connections);
+  console.log('Database connection successfull !!!');
+})
 const app = require('./app');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
